0ffc08b02a10cc18a1001a9d6f81e13453146c90
Ethan Weber <eweb0124@gmail.com>
2020-01-09 17:51:05 +0800

    Starting python code for prototyping effects.

---
+++ cv/README.md
@@ -0,0 +1,23 @@
+# cv (computer vision)
+
+Documentation for all CV stuff, including the image framework.
+
+# Setup
+
+```
+# create pip virtual environment
+python3 -m venv venv
+
+# start virtual environment
+source venv/bin/activate
+
+# close virtual environment
+deactivate
+
+# install packages
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# install Kernel to use with notebooks
+python -m ipykernel install --user --display-name "imagear"
+```
---
+++ cv/main.py
@@ -0,0 +1,53 @@
+"""
+Python playground for image effects.
+"""
+
+import cv2
+import argparse
+import sys
+
+
+class Effector(object):
+    """
+    The Effector class will perform effects on an input filepath.
+    Note that the Effector will call the same C++ functions that
+    the iOS application will call.
+    """
+    effects = {
+        "boomerang",
+        "hyperlapse",
+        "3d_ken_burns",
+        "2d_ken_burns",
+        "dolly",
+        "space_elevator"
+    }
+
+    def __init__(self, path, effect):
+        self.path = path
+        self.effect = effect
+
+        # Make sure that the specified effect is valid.
+        if self.effect not in self.effects:
+            raise AssertionError("{} not a valid effect.".format(self.effect))
+
+    def start_gui(self):
+        """
+        Load from filepath and start gui.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", type=str,
+                        help="Input to the program. Should be a filepath.")
+    parser.add_argument("--effect", type=str, default="3d_ken_burns",
+                        help="Effect to run the program with.")
+    args = parser.parse_args()
+
+    if not args.path:
+        print("Need to specify path. Exiting.")
+        sys.exit()
+
+    print("Running with filepath: {}".format(args.path))
+    effector = Effector(args.path, args.effect)
