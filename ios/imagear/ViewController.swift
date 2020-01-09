dd374e4d6afdb5d421bcd814c9481241422d8e81
Avery Lamp <averylamp@gmail.com>
2020-01-09 16:50:38 +0800

    Initial ios app

---
+++ ios/imagear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imagear
+//
+//  Created by Avery Lamp on 1/9/20.
+//  Copyright © 2020 MAE Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
