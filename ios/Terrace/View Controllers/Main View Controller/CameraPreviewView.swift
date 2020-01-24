9912c1bb2ee63849952264f86c2aa50e2a71783b
Avery Lamp <averylamp@gmail.com>
2020-01-24 20:47:11 +0800

    Unworking ios

---
+++ ios/Terrace/View Controllers/Main View Controller/CameraPreviewView.swift
@@ -0,0 +1,33 @@
+//
+//  CameraPreviewView.swift
+//  Terrace
+//
+//  Created by Avery Lamp on 1/23/20.
+//  Copyright © 2020 MAE Labs. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+        }
+        return layer
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+}
